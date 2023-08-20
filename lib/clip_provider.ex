2190272e11b90f1142da0e2cae151cb73f6ab136
David Gomes <10091092+LegendL3n@users.noreply.github.com>
2023-08-20 12:43:07 +0000

    refator: improve code quality

--- lib/clip_provider.ex
+++ lib/clip_provider.ex
@@ -1,20 +1,19 @@
 defmodule ClipProvider do
   @extract_clip_name ~r/.+\/\d\d\. (.+) -.+\.mp3/U
 
+  @spec get_random_clip :: [{:name, binary} | {:path, binary}]
   @doc """
   Returns a random clip from the clips/ folder, ignoring the first of a CD. (e.g. "01. Introdução")
   """
   def get_random_clip do
-    get_all_clips() |> ignore_intro_clips |> Enum.random |> build_clip
+    get_all_clips() |> Enum.filter(&is_not_intro_clip/1) |> Enum.random |> build_clip
   end
 
   defp get_all_clips do
     Path.wildcard("clips/*/*.mp3")
   end
 
-  defp ignore_intro_clips(clips) do
-    Enum.filter(clips, &(!(&1 =~ "01")))
-  end
+  defp is_not_intro_clip(clip), do: !(clip =~ "01")
 
   defp build_clip(path) do
     [name: get_clip_name(path), path: path]
