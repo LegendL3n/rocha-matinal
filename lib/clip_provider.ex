28e0b634f1949781175253f6e7f8d8584ee88728
David Gomes <10091092+LegendL3n@users.noreply.github.com>
2023-08-23 21:19:49 +0000

    fix: sanitize clip name

--- lib/clip_provider.ex
+++ lib/clip_provider.ex
@@ -1,5 +1,6 @@
 defmodule ClipProvider do
-  @extract_clip_name ~r/.+\/\d\d\. (.+) -.+\.mp3/U
+  @extract_clip_name_regex ~r/.+\/\d\d\. (.+) -.+\.mp3/U
+  @sanitize_regex ~r/[^a-zA-Z0-9]+/
 
   @spec get_random_clip :: [{:name, binary} | {:path, binary}]
   @doc """
@@ -10,7 +11,7 @@ defmodule ClipProvider do
   end
 
   defp get_all_clips do
-    Path.wildcard("clips/*/*.mp3")
+    Path.wildcard("./clips/Volume 3/12. Zero Mil, Zero Centos - Fernando Rocha - vol.3.mp3")
   end
 
   defp is_not_intro_clip(clip), do: !(clip =~ "01")
@@ -20,7 +21,7 @@ defmodule ClipProvider do
   end
 
   defp get_clip_name(path) do
-    [_, name] = Regex.run(@extract_clip_name, path)
-    name
+    [_, name] = Regex.run(@extract_clip_name_regex, path)
+    Regex.replace(@sanitize_regex, name, "_")
   end
 end
