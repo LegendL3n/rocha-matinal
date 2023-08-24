5d5dfa9bd0d5504ee2f6f29ea8e7725631b89086
David Gomes <10091092+LegendL3n@users.noreply.github.com>
2023-08-24 21:37:27 +0000

    feat: remove letter accents

--- lib/clip_provider.ex
+++ lib/clip_provider.ex
@@ -1,6 +1,6 @@
 defmodule ClipProvider do
   @extract_clip_name_regex ~r/.+\/\d\d\. (.+) -.+\.mp3/U
-  @sanitize_regex ~r/[^a-zA-Z0-9]+/
+  @sanitize_regex ~r/[^a-zA-Z0-9 -.]+/
 
   @spec get_random_clip :: [{:name, binary} | {:path, binary}]
   @doc """
@@ -17,11 +17,13 @@ defmodule ClipProvider do
   defp is_not_intro_clip(clip), do: !(clip =~ "01")
 
   defp build_clip(path) do
+    IO.puts "Building clip '#{path}'"
     [name: get_clip_name(path), path: path]
   end
 
   defp get_clip_name(path) do
+    path = path |> String.normalize(:nfd)
     [_, name] = Regex.run(@extract_clip_name_regex, path)
-    Regex.replace(@sanitize_regex, name, "_")
+    Regex.replace(@sanitize_regex, name, "")
   end
 end
