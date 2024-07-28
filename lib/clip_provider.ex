8f99624a0d1eda23691ee374cc7200123966562e
David Gomes <10091092+davidgomesdev@users.noreply.github.com>
2024-07-28 15:31:01 +0100

    feat: avoid sending duplicates

--- lib/clip_provider.ex
+++ lib/clip_provider.ex
@@ -2,28 +2,20 @@ defmodule ClipProvider do
   @extract_clip_name_regex ~r/.+\/\d\d\. (.+) -.+\.mp3/U
   @sanitize_regex ~r/[^a-zA-Z0-9 -.]+/
 
-  @spec get_random_clip :: [{:name, binary} | {:path, binary}]
-  @doc """
-  Returns a random clip from the clips/ folder, ignoring the first of a CD. (e.g. "01. Introdução")
-  """
-  def get_random_clip do
-    get_all_clips() |> Enum.filter(&is_not_intro_clip/1) |> Enum.random |> build_clip
+  def get_all_clips do
+    Path.wildcard("clips/*/*.mp3") |> Enum.filter(&is_not_intro_clip/1)
   end
 
-  defp get_all_clips do
-    Path.wildcard("clips/*/*.mp3")
-  end
-
-  defp is_not_intro_clip(clip), do: !(clip =~ "01")
-
-  defp build_clip(path) do
+  def build_clip(path) do
     IO.puts "Building clip '#{path}'"
     [name: get_clip_name(path), path: path]
   end
 
-  defp get_clip_name(path) do
+  defp is_not_intro_clip(clip), do: !(clip =~ "01")
+
+  def get_clip_name(path) do
     path = path |> String.normalize(:nfd)
     [_, name] = Regex.run(@extract_clip_name_regex, path)
-    Regex.replace(@sanitize_regex, name, "")
+    Regex.replace(@sanitize_regex, name, "") |> String.replace(" ", "_")
   end
 end
--- lib/rocha_matinal.ex
+++ lib/rocha_matinal.ex
@@ -1,16 +1,34 @@
-defmodule Mix.Tasks.RochaMatinal.Task do
-  use Mix.Task
-
+defmodule RochaMatinal.CLI do
   alias HTTPoison, as: HTTP
   alias ClipProvider
   alias DiscordSender
 
-  @spec run(any) :: :ok
-  def run(_) do
-    HTTP.start
+  def main(args) do
+    args = args |> parse_args
 
-    ClipProvider.get_random_clip |> DiscordSender.send_clip
+    HTTP.start()
 
-    :ok
+    sent_clips = DiscordSourcer.get_sent_clips(args[:channel_id])
+
+    unsent_clips =
+      ClipProvider.get_all_clips()
+      |> Enum.filter(fn clip_path ->
+        !Enum.any?(sent_clips, &(&1[:name] == ClipProvider.get_clip_name(clip_path)))
+      end)
+
+    if Enum.empty?(unsent_clips) do
+      IO.puts("All clips have been sent!!")
+      System.halt(1)
+    end
+
+    unsent_clips |> Enum.random() |> ClipProvider.build_clip() |> DiscordSender.send_clip(args[:webhook_url])
+  end
+
+  defp parse_args(args) do
+    {opts, _, _} =
+      args
+      |> OptionParser.parse(aliases: [w: :webhook_url, c: :channel_id], strict: [webhook_url: :string, channel_id: :integer])
+
+    opts
   end
 end
